7408cafbd91063065f2eb3192860584006430d31
andreasmuthig <155832695+andreasmuthig@users.noreply.github.com>
2025-07-19 21:37:28 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
+const container = document.getElementById('tiles');
+let isDown = false;
+let startX, scrollLeft;
 
-function scrollToSection(id) {
-    const section = document.getElementById(id);
-    section.scrollIntoView({ behavior: "smooth" });
-}
+container.addEventListener('mousedown', (e) => {
+  isDown = true;
+  container.classList.add('active');
+  startX = e.pageX - container.offsetLeft;
+  scrollLeft = container.scrollLeft;
+});
+container.addEventListener('mouseleave', () => {
+  isDown = false;
+  container.classList.remove('active');
+});
+container.addEventListener('mouseup', () => {
+  isDown = false;
+  container.classList.remove('active');
+});
+container.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - container.offsetLeft;
+  const walk = (x - startX) * 2;
+  container.scrollLeft = scrollLeft - walk;
+});
