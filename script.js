aa886b056b4d275801a4d671ed0fe969272c10fe
andreasmuthig <155832695+andreasmuthig@users.noreply.github.com>
2025-07-20 22:16:12 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,9 +2,5 @@ const contentBox = document.getElementById("contentBox");
 const texts = ['Ich bin Andreas, Technikliebhaber und Buchautor. Ich stelle die richtigen Fragen – auch an KI.', 'Vorlagen, Tools und Ideen, um ChatGPT besser zu nutzen.', 'Updates zu Projekten, Veröffentlichungen und Entwicklungen.', 'Kreative Prompt-Beispiele und interaktive Experimente mit GPT.', 'Live-Workshops zu KI, Fragen, Texten und Tools.', 'Blog-Artikel über kluge Fragen, moderne Tools und Strategien.', 'Leserstimmen und Rezensionen zu meinem Buch und Projekten.', 'Antworten auf häufige Fragen zur Technik der klugen Frage.'];
 
 function showContent(index) {
-  contentBox.classList.remove("visible");
-  setTimeout(() => {
-    contentBox.innerHTML = `<p>${texts[index]}</p>`;
-    contentBox.classList.add("visible");
-  }, 100);
+  contentBox.innerHTML = `<p>${texts[index]}</p>`;
 }
